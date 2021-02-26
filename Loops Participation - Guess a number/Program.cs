15134009b367c433f58e584abbbab2c2015a6c39
Alexander Upton <Alexander99W@ou.edu>
2021-02-25 23:10:29 -0600

    Finished, i think?

--- Loops Participation - Guess a number/Program.cs
+++ Loops Participation - Guess a number/Program.cs
@@ -9,11 +9,13 @@ namespace Loops_Participation___Guess_a_number
             Console.WriteLine("Set a MIN value");
             string answer = Console.ReadLine();
             int MIN = Convert.ToInt32(answer);
+            
 
 
             Console.WriteLine("Set a MAX value");
             answer = Console.ReadLine();
             int MAX = Convert.ToInt32(answer);
+            
 
             Console.WriteLine($"Guess the number between {MIN} and {MAX}");
             answer = Console.ReadLine();
@@ -22,6 +24,13 @@ namespace Loops_Participation___Guess_a_number
             Random rand = new Random();
             int randomNumber = rand.Next(MIN, MAX + 1);
 
+            while (randomNumber != guess)
+            {
+                Console.WriteLine("Your guess is incorrect, feel free to attempt again!");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine($"Hey good job, you got it right!");
 
 
 
